91d74c5636e09228fc2079301f2cb5483f80063b
rickyjames1750 <rickyjames1750@gmail.com>
2020-10-03 14:44:26 -0700

    This commit will implement code to generate random cards when button is clicked

--- SparksCardGame/ViewController.swift
+++ SparksCardGame/ViewController.swift
@@ -14,9 +14,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var leftImageView: UIImageView!
     
-    
     @IBOutlet weak var leftScoreLabel: UILabel!
-    
     
     @IBOutlet weak var rightScoreLabel: UILabel!
     
@@ -28,9 +26,10 @@ class ViewController: UIViewController {
 
     
     @IBAction func dealTapped(_ sender: Any) {
-        print("Deal Tapped!")
+        let leftNumber = Int.random(in: 2...14)
+        let rightNumber = Int.random(in: 2...14)
+        leftImageView.image = UIImage(named: "card\(leftNumber)")
+        rightImageView.image = UIImage(named: "card\(rightNumber)")
     }
     
-
 }
-
