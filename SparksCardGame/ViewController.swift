4e9af4687b610bee63a0d6236cd0a9ca106650df
rickyjames1750 <rickyjames1750@gmail.com>
2020-10-03 16:20:17 -0700

    This commit will submit the final code build

--- SparksCardGame/ViewController.swift
+++ SparksCardGame/ViewController.swift
@@ -18,6 +18,9 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var rightScoreLabel: UILabel!
     
+    var leftScore = 0
+    var rightScore = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -26,10 +29,29 @@ class ViewController: UIViewController {
 
     
     @IBAction func dealTapped(_ sender: Any) {
+        // Randomize some numbers
         let leftNumber = Int.random(in: 2...14)
         let rightNumber = Int.random(in: 2...14)
+        
+        // Update the image views
         leftImageView.image = UIImage(named: "card\(leftNumber)")
         rightImageView.image = UIImage(named: "card\(rightNumber)")
+        
+        // Compare the random numbers
+        if leftNumber > rightNumber {
+            // Left side wins!
+            leftScore += 1
+            leftScoreLabel.text = String(leftScore)
+            
+            rightScore += 1
+            rightScoreLabel.text = String(rightScore)
+        }
+        else if leftNumber < rightNumber {
+            // Right side wins!
+        }
+         else {
+            // Tie
+        }
     }
     
 }
